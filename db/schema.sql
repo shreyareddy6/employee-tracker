e922d842cfbfc45b4c0d40d9067f4fc05b4c07c0
shreyareddy <32663313+shreyareddy6@users.noreply.github.com>
2024-08-01 17:15:37 -0400

    Added Schema

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+-- Create db for employees tracker
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+\c employee_tracker;
+
+-- Create table for employee details
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+)
+
+-- Create table for role details
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department INTEGER,
+    FOREIGN KEY (department)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+)
+
+-- Create table for department details
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+)
